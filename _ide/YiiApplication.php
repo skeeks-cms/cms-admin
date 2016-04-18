9ee1abf9f4ce48f3b79b2b273a92e71a4846e4a1
Саша <semenov@skeeks.com>
2016-04-19 01:14:52 +0300

    ide

---
+++ _ide/YiiApplication.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The pseudo-only IDE tips
+ *
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010-2014 SkeekS (Sx)
+ * @date 12.11.2014
+ * @since 1.0.0
+ */
+
+namespace yii\web;
+use skeeks\cms\modules\admin\components\settings\AdminSettings;
+
+/**
+ * @property AdminSettings                  $admin
+ *
+ * @property \yii\web\User|UserIde                 $user
+ *
+ * Class Application
+ * @package yii\web
+ */
+class Application
+{}
+
