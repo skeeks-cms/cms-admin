0e16283001cbb1f0022c1167fc692115fb7456d4
Semenov <semenov@skeeks.com>
2016-09-09 18:52:31 +0300

    Adapting to new additional properties

--- widgets/filters/views/_related-fields.php
+++ widgets/filters/views/_related-fields.php
@@ -45,7 +45,7 @@ $form = $widget;
 
         <? if ($property->property_type == \skeeks\cms\relatedProperties\PropertyType::CODE_ELEMENT) :?>
            <?
-                $propertyType = $property->createPropertyType();
+                $propertyType = $property->handler;
                 $options = \skeeks\cms\models\CmsContentElement::find()->active()->andWhere([
                     'content_id' => $propertyType->content_id
                 ])->all();
